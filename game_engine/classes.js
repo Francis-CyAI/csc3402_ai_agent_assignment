a6033272ec96ce31a331e464b629dcb728104486
Francis Kalunga <kalungafrancis28@gmail.com>
2024-09-23 15:26:23 +0200

    Added Code Strucure file by Josh; started working on engine classes

--- game_engine/classes.js
+++ game_engine/classes.js
@@ -1,4 +1,15 @@
 /// This file contains all the classes partaining to the game engine
 
 
-/*   */
+/* Pieces  */
+class Piece {
+    constructor(id){
+        this.id = id
+    }
+}
+
+class Man extends Piece {
+    constructor(id) {
+        super.id = id
+    }
+}
