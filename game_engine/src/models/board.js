5fa41dc195895a5da31d6949acdc10b64aeb8429
Francis Kalunga <kalungafrancis28@gmail.com>
2024-09-23 17:37:05 +0200

    created separate file for the Square model

--- game_engine/src/models/board.js
+++ game_engine/src/models/board.js
@@ -1,18 +1,1 @@
-/// Source file for the board and squares.
-/**
- * Each square will keep id state of the piece occupying it.
- */
-
-
-export class Square {
-    /**
-     * 
-     * @param {*} id is the identifier of a square
-     * @param {*} adjacencyMatrix is an array of ids of squares adjacent to this square
-     */
-    constructor(id, adjacencyMatrix) {
-        this.id = id
-        this.adjacencyMatrix = adjacencyMatrix
-        this.pieceId
-    }
-}
+/// Source file for the board.
