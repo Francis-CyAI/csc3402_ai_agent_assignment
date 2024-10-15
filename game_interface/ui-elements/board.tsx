f58b5c1fcebcee2fa5df5e6bd072902c1306be38
Francis Kalunga <kalungafrancis28@gmail.com>
2024-10-15 20:51:28 +0200

    Added dynamic board creation logic; correct start script

--- game_interface/ui-elements/board.tsx
+++ game_interface/ui-elements/board.tsx
@@ -1,26 +1,42 @@
 'use client'
 
 import Square from "./square"
+import { createBoardSquares } from "@/util/board";
+import { sqrColor2 } from "@/contants";
 
 export default function Board() {
     return (
         <div className="board">
-            <Square 
-                id="sqr-1"
-                color="white"
-                onlick={() => {
-                    alert("Square just got clicked")
-                }}
-                initialState={
-                    {
-                        occupied: true,
-                        piece: {
-                            type: "man",
-                            color: "black" // tailwind style
+            {
+                /* 
+                <Square 
+                    id="sqr-1"
+                    color="white"
+                    onlick={() => {
+                        alert("Square just got clicked")
+                    }}
+                    initialState={
+                        {
+                            occupied: true,
+                            piece: {
+                                type: "man",
+                                color: "black" // tailwind style
+                            }
                         }
                     }
-                }
-            />
+                />
+                */
+               createBoardSquares(10, sqrColor2).map((sqrList, index) => (
+                <div key={index} className="square-row">
+                    {
+                        sqrList.map((sqr) => (
+                            sqr
+                        ))
+                    }
+                </div>
+               ))
+            }
+            
         </div>
     );
 }
