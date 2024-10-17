4df1fd83bfd9abc55f0cf4951915a8e63c831deb
Francis Kalunga <kalungafrancis28@gmail.com>
2024-10-17 14:02:24 +0200

    created board state type

--- game_interface/state/board-state.ts
+++ game_interface/state/board-state.ts
@@ -2,19 +2,21 @@
  * BoardState is a matrix state of the checkers board.
  * The cells of the matrix will contain each a Square, which will be either playable or not.
  * Each Square will have its relevant mehods.
+ * 
+ * This is the initial board state
  */
 
-// import Squ
+import { SquareType } from "@/types/square"
 
-let BoardState = [ // whole: column
-    [], // row. Max lenth = 10
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    []
+export type BoardType = [ //  whole: column
+    SquareType[], // row. Max length = 10
+    SquareType[],
+    SquareType[],
+    SquareType[],
+    SquareType[],
+    SquareType[],
+    SquareType[],
+    SquareType[],
+    SquareType[],
+    SquareType[]
 ]
