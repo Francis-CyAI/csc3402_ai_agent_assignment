f0b2a2975e917b2fc39531c41f753f1b7d6fabb8
kalungafrancis28 <kalungafrancis28@gmail.com>
2024-10-17 11:11:57 +0000

    restructured server.js source file

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3003;
 
 // Serve index.html at the root URL
 app.get('/', (req, res) => {
